efc8864dba40662ed50178c9c175a815129e0788
Tomasz Sanecki <tomasz@sanecki.net>
2021-04-22 07:00:06 +0200

    Cleanup formatting

--- Mouse.cpp
+++ Mouse.cpp
@@ -10,7 +10,7 @@ using std::cout; using std::endl;
 
 void Mouse::init() {
     int ret = libusb_init(&context);
-    if(ret < 0) {
+    if (ret < 0) {
         cout<<"Init Error "<<ret<<endl;
         return;
     }
@@ -24,7 +24,7 @@ void Mouse::init() {
 void Mouse::discoverDevices() {
     libusb_device** devs;
     ssize_t cnt = libusb_get_device_list(context, &devs);
-    if(cnt < 0) {
+    if (cnt < 0) {
         cout<<"Get Device Error"<<endl;
         return;
     }
@@ -33,11 +33,11 @@ void Mouse::discoverDevices() {
         libusb_device_descriptor desc;
         libusb_get_device_descriptor(devs[i], &desc);
         if (isCompatibleDevice(desc)) {
-            if(libusb_open(devs[i], &currentDevice) != 0)
+            if (libusb_open(devs[i], &currentDevice) != 0)
                 continue;
 
-            if(libusb_kernel_driver_active(currentDevice, 2) == 1)
-                if(libusb_detach_kernel_driver(currentDevice, 2) != 0) {
+            if (libusb_kernel_driver_active(currentDevice, 2) == 1)
+                if (libusb_detach_kernel_driver(currentDevice, 2) != 0) {
                     libusb_close(currentDevice);
                     continue;
                 }
@@ -58,11 +58,11 @@ void Mouse::discoverDevices() {
 }
 
 bool Mouse::isCompatibleDevice(libusb_device_descriptor &desc) {
-    if(desc.idVendor != A4TECH_VID)
+    if (desc.idVendor != A4TECH_VID)
         return false;
 
-    for(size_t i = 0; i < COMPATIBLE_PIDS_SIZE; ++i)
-        if(desc.idProduct == COMPATIBLE_PIDS[i])
+    for (size_t i = 0; i < COMPATIBLE_PIDS_SIZE; ++i)
+        if (desc.idProduct == COMPATIBLE_PIDS[i])
             return true;
 
     return false;
@@ -71,7 +71,7 @@ bool Mouse::isCompatibleDevice(libusb_device_descriptor &desc) {
 Mouse::~Mouse() {
     for (auto& dev : devices)
         libusb_close(dev.second);
-    if(context != nullptr)
+    if (context != nullptr)
         libusb_exit(context);
 }
 
@@ -79,11 +79,11 @@ int Mouse::setBackLightLevel(uint8_t level) {
     uint8_t data[72] = {A4TECH_MAGIC, BACKLIGHT_OPCODE,0x00,0x00,
                         BACKLIGHT_WRITE,0x00,0x00,0x00, level, 0x00, };
 
-    if(level < 0 || level > 3){
+    if (level < 0 || level > 3) {
         return -1;
     }
 
-    if(writeToMouse(data, sizeof(data)) < 0)
+    if (writeToMouse(data, sizeof(data)) < 0)
         return -2;
 
     return 0;
@@ -92,7 +92,7 @@ int Mouse::setBackLightLevel(uint8_t level) {
 int Mouse::writeToMouse(uint8_t data[], size_t size) {
     int res =  libusb_control_transfer(currentDevice,0x21,9,
                                        0x0307,2,data,size,10000);
-    if(res < 0){
+    if (res < 0){
         cout<<"Unnable to send command"<<endl;
         return -1;
     }
@@ -101,13 +101,13 @@ int Mouse::writeToMouse(uint8_t data[], size_t size) {
 }
 
 int Mouse::readFromMouse(uint8_t *request, size_t requestSize, uint8_t *response, size_t responseSize) {
-    if(writeToMouse(request, requestSize) < 0){
+    if (writeToMouse(request, requestSize) < 0) {
         return -1;
     }
 
     int res =  libusb_control_transfer(currentDevice,0xa1,1,
                                        0x0307,2,response,responseSize,10000);
-    if(res < 0){
+    if (res < 0){
         cout<<"Unnable to receive data"<<endl;
         return -2;
     }
@@ -179,7 +179,7 @@ void Mouse::listDevices() {
 }
 
 bool Mouse::selectDevice(int address) {
-    if(devices.count(address) == 0)
+    if (devices.count(address) == 0)
         return false;
 
     currentDevice = devices.at(address);
